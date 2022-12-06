c0f8019ec99befbb6fa8a8a1a09570765c5b6b78
hugo.oliveira <hugo.bernardoni@gmail.com>
2022-12-06 09:04:21 -0300

    Created Test

---
+++ API/Test/ChatTest.cs
@@ -0,0 +1,37 @@
+﻿using JobSity.Model.Helpers;
+using Xunit;
+
+namespace Test
+{
+    public class ChatTest
+    {
+        [Fact]
+        public void Should_Be_A_Stock_Command_Message()
+        {
+            var result = ChatHelper.IsCommand("/stock=aapl.us");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Should_Be_A_Unknown_Command_Message()
+        {
+            var result = ChatHelper.IsCommand("/stock=1");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Should_Be_A_Commom_Message()
+        {
+            var result = ChatHelper.IsCommand("Hello JobSity");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Should_Be_A_Valid_Command_Message()
+        {
+            var result = ChatHelper.GetValidCommandMessage("/stock=aapl.us");
+            Assert.Equal("aapl.us", result);
+        }
+       
+    }
+}
